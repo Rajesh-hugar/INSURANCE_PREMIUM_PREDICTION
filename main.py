af91463c0fb2b1a0698471f6115a1e39fba37cec
Rajesh-hugar <rajeshhugar220@gmail.com>
2023-07-22 21:15:26 +0530

    changes added

---
+++ main.py
@@ -0,0 +1,22 @@
+from Insurance.logger import logging
+from Insurance.exception import InsuranceException
+import os,sys
+
+def test_logger_and_exception():
+    try:
+        logging.info('Starting the test logger and the exception')
+        result = 3/0
+        
+        print(result)
+        
+        logging.info('Ending Pointof the test logger and the exception')
+    except Exception as e:
+        logging.debug(str(e))
+        raise InsuranceException(e,sys)
+    
+    
+    if __name__ =='__main__':
+        try:
+            test_logger_and_exception()
+        except Exception as e:
+            print(e)
