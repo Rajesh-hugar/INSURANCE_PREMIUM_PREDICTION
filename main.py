cb68663c73dd8d866a217f818e87200f760f012c
Rajesh-hugar <rajeshhugar220@gmail.com>
2023-07-23 15:04:33 +0530

    dataingestion file added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ filterwarnings('ignore')
 import sys
 from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
+from Insurance.components.data_ingestion import DataIngestion
 
 
 #print(dir(sys))
@@ -30,7 +31,12 @@ if __name__ =='__main__':
         # test_logger_and_exception()
        # get_collection_as_dataframe(database_name ="INSURANCE",collection_name ="INSURANCE_PROJECT")
         training_pipeline_config =config_entity.TrainingPipelineConfig()
-        data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config)
+        
+          
+        #data ingestion
+        data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config)
         print(data_ingestion_config.to_dict())
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
     except Exception as e:
-        print(e)
+        raise InsuranceException(e,sys)
